5f119a22459ded463558e974118765fd9d7ec66a
Vance Leon <vanceleon44@gmail.com>
2019-09-06 13:10:01 -0400

    writing basic html reesponse from server

--- server.js
+++ server.js
@@ -6,8 +6,18 @@ const http = require('http');
 
 const server = http.createServer(function(req, res) {
     console.log(req);
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>My first Page</title></head>');
+    res.write('<body><h1>Hello from my Node.js server!</h1></body>');
+    res.write('</html>');
+    res.end();
+
+    //process.exit();// this will end processes in the server exits the event loop
 });
 
-const port = process.env.PORT || 8000;
+server.listen(3000)
 
-server.listen(port, () => console.log(`=============Server on ${port}=============`));
+// const port = process.env.PORT || 8000;
+
+// server.listen(port, () => console.log(`=============Server on ${port}=============`));

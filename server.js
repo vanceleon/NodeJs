a66bb3ef329e7954ad1c9edbfeb57395a7789d90
Vance Leon <vanceleon44@gmail.com>
2019-09-06 13:35:59 -0400

    vanilla nodejs and writing text files

--- server.js
+++ server.js
@@ -1,22 +1,51 @@
 const http = require('http');
-
+const fs = require('fs');
 // function rqListener(req, res) {
 
 // }
 
 const server = http.createServer(function(req, res) {
-    console.log(req);
+  const url = req.url;
+  const method = req.method;
+  if (url === '/') {
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
-    res.write('<head><title>My first Page</title></head>');
-    res.write('<body><h1>Hello from my Node.js server!</h1></body>');
+    res.write('<head><title>Enter Messenge</title></head>');
+    res.write(
+      '<body><form action="/message" method="POST"/><input type="text" name="message"/><button type="submit">Send</button></form></body>'
+    );
     res.write('</html>');
-    res.end();
+    return res.end();
+  }
 
-    //process.exit();// this will end processes in the server exits the event loop
+  if (url === '/message' && method === 'POST') {
+    const body = [];
+    req.on('data', chunk => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+    req.on('end', () => {
+      const parsedBody = Buffer.concat(body).toString();
+      console.log(parsedBody);
+      const message = parsedBody.split('=')[1];
+      fs.writeFileSync('message.txt', message);
+    });
+    res.statusCode = 302;
+    res.setHeader('Location', '/');
+    return res.end();
+  }
+  res.setHeader('Content-Type', 'text/html');
+  res.write('<html>');
+  res.write('<head><title>Enter Messenge</title></head>');
+  res.write('<body><h1>Testing the input page</h1></body>');
+  res.write('</html>');
+  res.end();
+  // console.log(req);
+
+  //process.exit();// this will end processes in the server exits the event loop
 });
 
-server.listen(3000)
+server.listen(3000);
 
 // const port = process.env.PORT || 8000;
 

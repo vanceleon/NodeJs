c56c3bde3cdbcabcb7b89ffcacc9e8276cc01e91
Vance Leon <vanceleon44@gmail.com>
2019-09-10 13:19:42 -0400

    styling

--- routes/admin.js
+++ routes/admin.js
@@ -1,13 +1,13 @@
 const router = require('express').Router();
 const bodyParser = require('body-parser');
+const rootDir = require('../util/path');
 const path = require('path');
-// const path = require('path');
 
 router.use(bodyParser.urlencoded({extended: false}));
 
 router.get('/add-product', (req, res, next) => {
   // console.log("another middleware product")
-  res.sendFile(path.join(__dirname, '../', 'views', 'addProduct.html'));
+  res.sendFile(path.join(rootDir, 'views', 'addProduct.html'));
 }); //add new middleware function
 
 router.post('/add-product', (req, res, next) => {
--- routes/shop.js
+++ routes/shop.js
@@ -1,11 +1,12 @@
 const router = require('express').Router();
 const path = require('path');
+const rootDir = require('../util/path');
 
 router.get('/', (req, res, next) => {
     // console.log("another middleware")
     // res.send('<h1>Hello from express</h1>');
     
-  res.sendFile(path.join(__dirname,'../','views', 'shop.html'));
+  res.sendFile(path.join(rootDir,'views', 'shop.html'));
 }); //add new middleware function
 
 module.exports = router;
